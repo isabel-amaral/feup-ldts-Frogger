492eb01cd5187c511788fc1d8e945c7fb13ead2b
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-01-06 15:00:05 +0000

    Added position getters and setters and made a correction in moveUp

--- src/main/java/Frog.java
+++ src/main/java/Frog.java
@@ -10,11 +10,17 @@ public class Frog {
         this.position = new Position(x, y);
     }
 
+    public Position getPosition() {
+        return position;
+    }
+    public void setPosition(Position position) {
+        this.position = position;
+    }
     public Position moveUp(){
-        return new Position(position.getX(), position.getY() - 1);
+        return new Position(position.getX(), position.getY() + 1);
     }
     public Position moveDown(){
-        return new Position(position.getX(), position.getY() + 1);
+        return new Position(position.getX(), position.getY() - 1);
     }
     public Position moveLeft(){
         return new Position(position.getX() - 1, position.getY());
