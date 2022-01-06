d0bf9557adf7e62f2558aba686b562f3a31e1caf
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-01-06 16:40:54 +0000

    Created class PositionRange

---
+++ src/main/java/PositionRange.java
@@ -0,0 +1,16 @@
+public class PositionRange {
+    int yMin;
+    int yMax;
+
+    public PositionRange(int min, int max) {
+        this.yMin = min;
+        this.yMax = max;
+    }
+
+    public int getyMin() {
+        return yMin;
+    }
+    public int getyMax() {
+        return yMax;
+    }
+}
