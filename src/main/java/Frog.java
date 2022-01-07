26822cb2fdac2f9f3a776423a29cd88057e201fb
Anete <anetempereira@gmail.com>
2022-01-07 20:46:37 +0000

    Some corrections in class Frog

--- src/main/java/Frog.java
+++ src/main/java/Frog.java
@@ -10,16 +10,16 @@ public class Frog extends MovableElement{
     }
 
     public Position moveUp(){
-        return new Position(getPosition().getX(), getPosition().getY() + 1);
+        return new Position(position.getX(), position.getY() + 1);
     }
     public Position moveDown(){
-        return new Position(getPosition().getX(), getPosition().getY() - 1);
+        return new Position(position.getX(), position.getY() - 1);
     }
     public Position moveLeft(){
-        return new Position(getPosition().getX() - 1, getPosition().getY());
+        return new Position(position.getX() - 1, position.getY());
     }
     public Position moveRight() {
-        return new Position(getPosition().getX() + 1, getPosition().getY());
+        return new Position(position.getX() + 1, position.getY());
     }
 
     public void draw(TextGraphics graphics){
