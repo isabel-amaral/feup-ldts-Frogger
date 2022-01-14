10d3653309efd29628406bcdb80ff559f5459bfd
Anete <anetempereira@gmail.com>
2022-01-14 23:39:29 +0000

    Corrections in MoveLeft and MoveRight

--- src/main/java/pt/up/fe/ldts/frogger/MoveRight.java
+++ src/main/java/pt/up/fe/ldts/frogger/MoveRight.java
@@ -3,6 +3,8 @@ package pt.up.fe.ldts.frogger;
 public class MoveRight extends Command{
 
     public Position execute(Position position) {
+        if (position.getX() == 60)
+            return new Position(1, position.getY());
         return new Position(position.getX() + 1, position.getY());
     }
 }
