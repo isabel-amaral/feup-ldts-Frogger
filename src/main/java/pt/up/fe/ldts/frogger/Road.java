f6d5d5812c45855c813fddfed3c73de6b86f0b77
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-01-18 14:19:50 +0000

    Created new non movable element, road

---
+++ src/main/java/pt/up/fe/ldts/frogger/Road.java
@@ -0,0 +1,20 @@
+package pt.up.fe.ldts.frogger;
+
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public class Road extends NonMovableElement {
+    public Road(int min, int max) {
+        super(min, max);
+    }
+
+    @Override
+    public void draw(TextGraphics graphics) {
+        for (int y = position.getYMin(); y <= position.getYMax(); y++) {
+            graphics.setForegroundColor(TextColor.Factory.fromString("#FFFFFF"));
+            for (int x = 0; x <= 59; x++)
+                graphics.putString(new TerminalPosition(x, y), String.valueOf(' '));
+        }
+    }
+}
